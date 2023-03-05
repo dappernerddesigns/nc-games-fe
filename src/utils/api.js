b242be7b92ef85937a368b3b1db84d0c45d6996e
Verity Gregory <verity.gregory@gmail.com>
2023-03-05 16:49:08 +0000

    updated api url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,78 +1,78 @@
-const axios = require('axios')
+const axios = require("axios");
 
 const gamesApi = axios.create({
-  baseURL: 'https://dappernerddesigns-nc-games.herokuapp.com/api',
-})
+    baseURL: "https://dapper-nerd-designs-nc-games.onrender.com/api",
+});
 
 export const AllCategories = () => {
-  return gamesApi.get(`/categories`).then((res) => {
-    return res.data.categories
-  })
-}
+    return gamesApi.get(`/categories`).then((res) => {
+        return res.data.categories;
+    });
+};
 
 export const AllReviewsByQuery = (query) => {
-  return gamesApi.get(`/reviews${query}`).then((res) => {
-    return res.data.reviews
-  })
-}
+    return gamesApi.get(`/reviews${query}`).then((res) => {
+        return res.data.reviews;
+    });
+};
 
 export const AllUsers = () => {
-  return gamesApi.get(`/users`).then((res) => {
-    return res.data.users
-  })
-}
+    return gamesApi.get(`/users`).then((res) => {
+        return res.data.users;
+    });
+};
 
 export const GetUser = (user) => {
-  return gamesApi.get(`/users/${user}`).then((res) => {
-    console.log(res.data)
-    return res.data.users
-  })
-}
+    return gamesApi.get(`/users/${user}`).then((res) => {
+        console.log(res.data);
+        return res.data.users;
+    });
+};
 
 export const ReviewByID = (reviewID) => {
-  return gamesApi.get(`/reviews/${reviewID}`).then((res) => {
-    return res.data.review[0]
-  })
-}
+    return gamesApi.get(`/reviews/${reviewID}`).then((res) => {
+        return res.data.review[0];
+    });
+};
 
 export const CommentsByReview = (reviewID) => {
-  return gamesApi.get(`/reviews/${reviewID}/comments`).then((res) => {
-    return res.data.comments
-  })
-}
+    return gamesApi.get(`/reviews/${reviewID}/comments`).then((res) => {
+        return res.data.comments;
+    });
+};
 
 export const ReviewVotes = (reviewID, num) => {
-  return gamesApi
-    .patch(`/reviews/${reviewID}`, { inc_votes: num })
-    .then((res) => {
-      return res.data.reviews[0].votes
-    })
-}
+    return gamesApi
+        .patch(`/reviews/${reviewID}`, { inc_votes: num })
+        .then((res) => {
+            return res.data.reviews[0].votes;
+        });
+};
 
 export const commentVoting = (commentID, num) => {
-  return gamesApi
-    .patch(`/comments/${commentID}`, { inc_votes: num })
-    .then((res) => {
-      return res.data.comments
-    })
-}
+    return gamesApi
+        .patch(`/comments/${commentID}`, { inc_votes: num })
+        .then((res) => {
+            return res.data.comments;
+        });
+};
 
 export const AddComment = (reviewID, comment) => {
-  return gamesApi
-    .post(`/reviews/${reviewID}/comments`, {
-      username: 'jessjelly',
-      body: comment,
-    })
-    .then((res) => {
-      return res.data.comments
-    })
-    .catch((err) => {
-      console.dir(err)
-    })
-}
+    return gamesApi
+        .post(`/reviews/${reviewID}/comments`, {
+            username: "jessjelly",
+            body: comment,
+        })
+        .then((res) => {
+            return res.data.comments;
+        })
+        .catch((err) => {
+            console.dir(err);
+        });
+};
 
 export const RemoveComment = (comment_id) => {
-  return gamesApi.delete(`/comments/${comment_id}`).then((res) => {
-    return res.data
-  })
-}
+    return gamesApi.delete(`/comments/${comment_id}`).then((res) => {
+        return res.data;
+    });
+};
